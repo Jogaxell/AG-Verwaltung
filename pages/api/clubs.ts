6ca400e1b918472877d6cf41df981672bdc51bdb
jogaxel <jogaxel@web.de>
2022-07-10 09:45:08 +0200

    club api route ready

--- pages/api/clubs.ts
+++ pages/api/clubs.ts
@@ -1,47 +1,55 @@
-import { unstable_getServerSession } from "next-auth/next"
+import {unstable_getServerSession} from "next-auth/next"
 
-import { NextApiResponse, NextApiRequest } from "next";
+import {NextApiRequest, NextApiResponse} from "next";
 
 // @ts-ignore
-import { authOptions } from '/pages/api/auth/[...nextauth]'
+import {authOptions} from '/pages/api/auth/[...nextauth]'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method === "POST") {
-        if(!req.body) {
-            res.status(400).send({success: false, error: {message: 'Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.'}})
+    if (req.method === "POST") {
+        if (!req.body) {
+            res.status(400).send({
+                success: false,
+                error: {message: 'Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.'}
+            })
             return
         }
-
         //TODO: handle editing with id
-
-        const { titel, description, grade, date, lesson, room, teacher, talentPromotion, active } = req.body
-
-        console.log(req.body)
-
-        res.status(404).send({success: true, error: {message: 'Es hat Funktioniert!'}})
-        return
-
-        // Optional logging to see the responses
-        // in the command line where next.js app is running.
 
         const session = await unstable_getServerSession(req, res, authOptions)
 
-
-
-        //TODO: look if user actualy has permissions to edit/create club
+        //TODO: look if user actually has permissions to edit/create club
         if (!session) {
-            // @ts-ignore
-            res.status(401).json({ message: "You must be logged in." });
+            res.status(401).json({message: "You must be logged in."});
             return;
         }
 
-        // @ts-ignore
-        return res.status(200).json({
-            message: 'Success',
-            data: {
-            }
+
+        let {titel, description, grade, date, lesson, room, teacher, talentPromotion, active} = req.body
+        if (!titel || !description || !grade || !date || !lesson || !room || !teacher || talentPromotion == undefined || active == undefined) {
+            res.status(400).send({success: false, error: {message: 'Nicht alle Felder wurden ausgefüllt.'}})
+            return
+        }
+        teacher = teacher.split(",").map((t: string) => t.trim())
+        if (teacher.length == 0) {
+            res.status(400).send({success: false, error: {message: 'Nicht alle Felder wurden ausgefüllt.'}})
+            return
+        }
+        const Club = require('../../models/club')
+        const createdClub = await Club.create({
+            name: titel,
+            description: description,
+            grade: grade,
+            date: date,
+            lesson: lesson,
+            room: room,
+            teacher: teacher,
+            talentPromotion: talentPromotion,
+            active: active
         })
-    }else {
+
+        return res.status(200).json({message: 'Success'})
+    } else {
         // @ts-ignore
         res.status(405).send("Method not allowed");
     }
