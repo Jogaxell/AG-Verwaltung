eed2a4e01d55a7d95bc843a40ac506e4500db9a0
jogaxel <jogaxel@web.de>
2022-07-16 11:02:23 +0200

    using next link component for sidebar

--- components/adminSidebarEntry.tsx
+++ components/adminSidebarEntry.tsx
@@ -1,4 +1,5 @@
 import {useRouter} from "next/router";
+import Link from "next/link";
 
 // @ts-ignore
 export default function AdminSidebarEntry({name, link, icon, addition}) {
@@ -9,7 +10,7 @@ export default function AdminSidebarEntry({name, link, icon, addition}) {
             <div>
                 <a href={link} style={style} className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100">
                     {icon}
-                    <span className="ml-3">{name}</span>
+                    <Link href={link} className="ml-3">{name}</Link>
                     {addition}
                 </a>
             </div>
