ae63c7650fef25d2af8fde814b56281dd814cadd
jogaxel <jogaxel@web.de>
2022-09-04 13:57:39 +0200

    type short in date formatter

--- components/dateFormatter.tsx
+++ components/dateFormatter.tsx
@@ -1,24 +1,23 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import { Button } from '@mantine/core';
-import {resize} from "next/dist/server/lib/squoosh/impl";
-
-export default function dateFormatter({date}: {date: string | string[]}) {
+export default function dateFormatter({date, type}: { date: string | string[], type: string | null }) {
     const dates = [
         {label: "Montag", value: "1"},
-    {label: "Dienstag", value: "2"},
-    {label: "Mittwoch", value: "3"},
-    {label: "Donnerstag", value: "4"},
-    {label: "Freitag", value: "5"},
-    {label: "14-tägig", value: "8"},
-    {label: "nach Vereinbarung", value: "9"}]
+        {label: "Dienstag", value: "2"},
+        {label: "Mittwoch", value: "3"},
+        {label: "Donnerstag", value: "4"},
+        {label: "Freitag", value: "5"},
+        {label: "14-tägig", value: "8"},
+        {label: "nach Vereinbarung", value: "9"}]
 
     let result: string = ""
-    if(date instanceof Array) {
+    if (date instanceof Array) {
         result = date.map(d => dates.find(d2 => d2.value === d)?.label).join(", ")
-    }else {
+    } else {
         result = dates.find(d => d.value === date)?.label as string
     }
 
+    if (type === "short") {
+        result = result.substring(0, 2)
+    }
     return (
         <>{result}</>
     )
